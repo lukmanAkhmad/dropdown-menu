b5988374d6f18897cad55bc71636241c132eb10d
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-05-25 13:09:38 +0700

    style: section btn

--- src/dropdown-menu.js
+++ src/dropdown-menu.js
@@ -1,3 +1,5 @@
+import dropdownIcon from "./assets/img/arrow_drop_down.svg";
+
 export default function createDropdown({
   containerElementCSSSelector = "body",
   itemDropdown = ["Option 1"],
@@ -7,17 +9,23 @@ export default function createDropdown({
 
   const dropdown = document.createElement("div");
   dropdown.classList.add("dropdown");
+  const sectionBtn = document.createElement("section");
+  sectionBtn.classList.add("section-btn");
   const btnDropdown = document.createElement("button");
   btnDropdown.classList.add("btn-dropdown");
   btnDropdown.textContent = "Dropdown";
+  const iconDropdown = document.createElement("img");
+  iconDropdown.classList.add("icon-dropdown");
+  iconDropdown.src = dropdownIcon;
+  iconDropdown.alt = "Dropdown icon";
+  iconDropdown.title = "Dropdown icon";
   const dropdownContent = document.createElement("ul");
   dropdownContent.classList.add("dropdown-content");
 
   const showDropdownContent = () => dropdownContent.classList.toggle("visible");
   const hideDropdownContent = () => dropdownContent.classList.remove("visible");
 
-  btnDropdown.addEventListener(expandEvent, (e) => {
-    e.preventDefault();
+  btnDropdown.addEventListener(expandEvent, () => {
     if (dropdownContent.classList.contains("visible")) {
       hideDropdownContent();
     } else {
@@ -32,7 +40,9 @@ export default function createDropdown({
     dropdownContent.appendChild(dropdownItem);
   });
 
-  dropdown.appendChild(btnDropdown);
+  sectionBtn.appendChild(btnDropdown);
+  sectionBtn.appendChild(iconDropdown);
+  dropdown.appendChild(sectionBtn);
   dropdown.appendChild(dropdownContent);
   containerElement.appendChild(dropdown);
 }
